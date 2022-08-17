6149157ef7f43925bdbc4e22b1f4d5330cd660a3
qilixiang <huahuaysyn@163.com>
2022-08-17 16:25:05 +0800

    配置项目

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,2 @@
-import { createStore } from 'vuex'
-
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+import { createPinia } from 'pinia'
+export const pinia = createPinia()
